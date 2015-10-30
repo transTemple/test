30ebff65b6b3f89dbc709dd981c15667b4aa98c4
Temple <trans.temple@gmail.com>
2015-10-31 00:08:09 +0900

    test

---
+++ yaruo_files/ajs.php
@@ -0,0 +1,10 @@
+(function(){
+var s = "<"+"script type=\'text/javascript\'>\n";
+s += "returnAD();\n";
+s += "<"+"/script>\n";
+s += "<"+"div id=\'beacon_edd1e26b64\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"http://cat.jp.as.criteo.com/delivery/lg.php?cppv=1&cpp=c5cLVHxKME9hOVorWDhxOXkvaXBGZ00yd29ZN05ueFBKRVltT29sVDJWUm83UE1pNnVmYlY4Zm81YksrWXVGbVMvMURjQmt2TW01NXBDMEJMWFpqczUzM3daV1dmTEFHYTN0Z2tBWkNST3p0K1E0STFOV0c0QVFNbW14ZHVaak1mR2lWM3pSb1VSQ1F0L0FNTnhxQmZlZVVEYmIxaG9sc1h3Qy9ld0ZpTVBaZ1kzb2l6RkY4RDlZNHF3cUk5QVhWWjFYemV8\"/>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"http://sync.mathtag.com/sync/img?type=sync&mt_exid=20&redir=http%3a%2f%2fdis.criteo.com%2frex%2fmatch.aspx%3fc%3d2%26uid%3d%5bMM_UUID%5d\"/>\n";
+s += "<"+"/div>\n";
+s += "\n";
+document.write(s);})();
